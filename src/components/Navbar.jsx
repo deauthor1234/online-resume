e1ec1f781b9e884b59d8c7faadd081fc40f17c7e
Oladapo Authority <dimejioladapo573@gmail.com>
2025-05-01 15:53:51 +0100

    built

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import SocialMediaIcons from './SocialMediaIcons';
 import Btn from './Btn';
-import { BsList } from 'react-icons/bs';
+import { BsList, BsX } from 'react-icons/bs';
 
 const Navbar = () => {
   // Getting the nav and navbar min-width values
@@ -13,10 +13,16 @@ const Navbar = () => {
     setNavWidth(navbarWidth)
   });
 
+  const [navShow, setNavShow] = useState(false);
+
+  const handleClick = () => {
+    !navShow ? setNavShow(true) : setNavShow(false);
+  }
+
   return (
-    <nav style={{minWidth: `${navWidth}`}}>
+    <nav className={navShow && "show"} style={{minWidth: `${navWidth}`}}>
         <div className="navbar" style={{minWidth: `${navbarWidth}`}}>
-          <BsList id='menuIcon' />
+          {!navShow ? <BsList id='menuIcon' onClick={handleClick} /> : <BsX id='menuIcon' className='x' onClick={handleClick} />}
           <div className="navbar_content">
             <p className="logo" data-aos="fade-down">Royal<span>Art</span></p>
             <div className="navlinks" data-aos="fade-right">
